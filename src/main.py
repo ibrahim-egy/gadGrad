76b24413ad462541809fee7a96ef0e750d0463d4
ibrahim-egy <ibrahimtarek.h@gmail.com>
2023-07-04 22:30:07 +0300

    Testing changes

--- src/main.py
+++ src/main.py
@@ -11,6 +11,9 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 Session(app)
 
+# =================================================#
+# this is the changesss
+# =================================================#
 
 @app.route('/')
 def home():
